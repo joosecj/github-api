aace921e8152857fbfc17a17ab3ae4a1fa21fa69
Jose Carlos <josecarloscjj@gmail.com>
2022-12-15 20:42:25 -0300

    fix: Route Home and HomeIndex

--- src/routes/Home/HomeIndex/index.tsx
+++ src/routes/Home/HomeIndex/index.tsx
@@ -5,8 +5,8 @@ import './styles.css';
 export default function HomeIndex() {
   return (
     <main>
-      <section className='card-home-index-content'>
-        <div className='container'>
+      <section className='card-home-index-content mr-top '>
+        <div className='container mr-bottom'>
           <CardHome title="Desafio GitHub" subtitle="DevSuperior - Escola de Programação" />
         </div>
         <div className='container btn-container'>
