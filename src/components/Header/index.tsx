8ba77244e1f609d31146dc1e04c88f3a05cb918f
Jose Carlos <josecarloscjj@gmail.com>
2022-12-15 19:12:08 -0300

    fix: component Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+export default function Header() {
+  return (
+    <header>
+      <div className='container title-header'>
+        <h1>Github API</h1>
+      </div>
+    </header>
+  );
+}
