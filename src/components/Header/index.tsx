999146773d698023cbca54221953fde1f581dbb7
Jose Carlos <josecarloscjj@gmail.com>
2023-01-06 16:59:31 -0300

    refactor: Link to home Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 export default function Header() {
   return (
     <header>
       <div className='container title-header'>
-        <h1>Github API</h1>
+        <Link to='/home'>
+          <h1>Github API</h1>
+        </Link>
       </div>
     </header>
   );
